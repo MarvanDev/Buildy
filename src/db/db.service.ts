6b1c0d75c59370f5cebc80ed4c4aa1b99f6097d5
fdramos <fdramos@unillanos.edu.co>
2026-04-22 19:08:56 -0500

    db.service.ts y history.repository.ts desarrollados

---
+++ src/db/db.service.ts
@@ -0,0 +1,7 @@
+// src/db/db.service.ts
+// Aquí iría la inicialización de tu DB (ej. initializeApp de Firebase)
+// Por ahora (KISS), actúa como un placeholder limpio.
+export const dbConnection = {
+  isConnected: true,
+  log: () => console.log('DB Conectada exitosamente'),
+};
